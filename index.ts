3fbcddf6ab7f83a9487093094c15f4f606d5082d
NikhilMudgal <NikhilMudgal@users.noreply.github.com>
2023-09-09 18:35:33 +0530

    Multiple Subscriptions

--- index.ts
+++ index.ts
@@ -15,9 +15,17 @@ const observable$ = new Observable<string>((subsciber) => {
 
 //Shorthand property of above observable$.subscribe((value) => console.log(value))
 
-const subscription = observable$.subscribe((value) => console.log(value)); // subscribe method return Subscription type
+// const subscription = observable$.subscribe((value) => console.log(value)); // subscribe method return Subscription type
+
+// setTimeout(() => {
+//   console.log('Unsubscribe');
+//   subscription.unsubscribe();
+// }, 3000);
+
+console.log('Subscription 1 starts');
+observable$.subscribe((value) => console.log('Subscription 1:', value));
 
 setTimeout(() => {
-  console.log('Unsubscribe');
-  subscription.unsubscribe();
-}, 3000);
+  console.log('Subscription 2 starts');
+  observable$.subscribe((value) => console.log('Subscription 2:', value));
+}, 1000);
